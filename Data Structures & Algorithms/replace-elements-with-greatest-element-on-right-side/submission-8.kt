14d89330c08efaabe4c641cb0a698dd57a4c0371
Callum White <callumw1991@gmail.com>
2026-05-10 13:28:47 +1000

    Add: replace-elements-with-greatest-element-on-right-side - submission-8

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-8.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun replaceElements(arr: IntArray): IntArray {
+        arr.forEachIndexed { index, i ->
+            if (arr.getOrNull(index + 1) != null) {
+                arr[index] = arr.sliceArray((index + 1)..(arr.size - 1)).max()
+            } else {
+                arr[index] = -1
+            }
+        }
+        return arr
+    }
+}
