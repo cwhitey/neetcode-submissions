5e476ce673dbdc236110d50e6e0c1f16d27a77d4
Callum White <callumw1991@gmail.com>
2026-05-11 21:01:58 +1000

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val trackerMap = mutableMapOf<Int, Int>()
+        nums.forEach { i ->
+            trackerMap[i] = trackerMap.getOrDefault(i, 0) + 1
+        }
+
+        println(trackerMap)
+
+        return trackerMap.toList().sortedBy { (_, c) -> -c }.take(k).map { (k, _) -> k }.toIntArray()
+    }
+}
